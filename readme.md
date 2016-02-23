c2121f9b6cc82c3272706f8b25d841b0d97e4b18
magnus udbjorg <magnus@udbjorg.net>
2016-02-23 18:38:50 +0100

    fixes

--- readme.md
+++ readme.md
@@ -5,11 +5,21 @@
 * NodeJs
 * Team Development for Sitecore
 
-##Setting up
+##Getting started
+
+##Sitecore
+1. Download Sitecore 8.1 rev. 151207 from http://dev.sitecore.net
+2. Extract Website into Wesite src/Website
+3. Attach Master as sitecore_CSP_Master
+4. Attach Core as sitecore_CSP_Core
+
 ###Build Frontend
 pm> npm install
 pm> webpack
 
-###Sync Sitecore (ctrl+F5)
+
+###Sync Sitecore
+1. Run application (ctrl+f5)
+2. Sync TDS
 
 ###browse to http://localhost:49914/
---
+++ src/Website/App_Start/Global.cs
@@ -0,0 +1,15 @@
+﻿using NWebsec.Csp;
+
+namespace NWebSecDemo.App_Start
+{
+    public class Global : Sitecore.Web.Application
+    {
+        protected void NWebSecHttpHeaderSecurityModule_CspViolationReported(object sender, CspViolationReportEventArgs e)
+        {
+            var report = e.ViolationReport;
+            string msg =
+                $"CSP - Blocked : {report.Details.BlockedUri}, which violated : {report.Details.ViolatedDirective} and Referrer : {report.Details.Referrer}";
+            Sitecore.Diagnostics.Log.Warn(msg, sender);
+        }
+    }
+}
